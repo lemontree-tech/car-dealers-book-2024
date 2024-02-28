5be3b4df594dc03b831ecc8d0a9498ee224f568c
falcon <falcon.lin.development@gmail.com>
2024-02-28 19:56:18 +0800

    updated image picker

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
       title: "Car Gallery",
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         // useMaterial3: false,
       ),
